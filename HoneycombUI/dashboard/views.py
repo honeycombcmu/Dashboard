daf24b0005350df53779daccb871ace323722024
chouclee <chouclee@cmu.edu>
2016-04-03 21:02:27 -0400

    retreive info from BIC cluster

--- HoneycombUI/dashboard/views.py
+++ HoneycombUI/dashboard/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 def index(request):
-    context = {}
-    return render(request, 'dashboard/index.html', context)
+	r = requests.get('http://128.2.7.38:7000/api/v1/clusters/legend/services/HDFS/components/DATANODE', 
+		auth=('admin','ravi666!'))
+	context = {}
+	if r.status_code == 200:
+		context = {'ServiceComponentInfo': r.json()['ServiceComponentInfo']}
+	return render(request, 'dashboard/index.html', context)
